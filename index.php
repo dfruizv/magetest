d5c5a8b885c9d7856646af902117a24030cc7a12
diego <diego@git.com>
2015-06-11 11:53:50 -0500

    Last minute changes

--- index.php
+++ index.php
@@ -4,13 +4,10 @@ echo "This is a test";
 
 <html>
 <head>
-<link type='text/css' rel='stylesheet' href='css/modernstyle.css'>
-<title>Index file</title>
-
+	<link type='text/css' rel='stylesheet' href='css/modernstyle.css'>
+	<title>Index file</title>
 </head>
 <body>
-<div class="container">
-HELLO
-</div>
+	<div class="container">HELLO</div>
 </body>
 </html>
